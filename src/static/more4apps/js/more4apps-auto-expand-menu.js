786f2e41f73185340a2260a39d627164e60b0ed8
Steve West <steve.west@more4appsnzltd.onmicrosoft.com>
2025-05-29 11:13:05 +1200

    Removed the js

--- src/static/more4apps/js/more4apps-auto-expand-menu.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const componentMenuToggle = document.querySelector('.nav-component .nav-toggle')
-  if (componentMenuToggle && componentMenuToggle.getAttribute('aria-expanded') === 'false') {
-    componentMenuToggle.click()
-  }
-})
